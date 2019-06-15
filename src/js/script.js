217d785fda2f4304322f9d6d6362e3cda0a4ab79
velmoren <velmoren@yahoo.com>
2019-06-15 19:48:25 +0300

    Практическая работа 1

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let money,
+    getStringArr = [],
     start = function() {
         do {
             money = prompt('Ваш месячный доход?', 13000);
@@ -19,42 +20,61 @@ let appData = {
     expenses: {},
     addExpenses: [],
     deposit: false,
+    percentDeposit: 0,
+    moneyDeposit: 0,
     mission: 75000,
     period: 3,
     budget: money,
     budgetDay: 0,
     budgetMonth: 0,
     expensesMonth: 0,
-    // метод сохранения ответов на вопросы о возможных расходах
+    // метод сохранения ответов на вопросы о возможных расходах и доходах
     asking: function() {
+
+        if (confirm('Есть ли у вас дополнительный заработок?')) {
+            let itemIncome;
+            let cashIncome;
+            do {
+                itemIncome = prompt('Какой у вас есть дополнительный заработок?', 'Таксую');
+            }
+            while (Number(itemIncome) || itemIncome === '' || itemIncome === null);
+            do {
+                cashIncome = prompt('Сколько в месяц вы на этом зарабатываете?', 10000);
+            }
+            while (isNaN(cashIncome) || cashIncome === '' || cashIncome === null);
+
+            appData.income[itemIncome] = cashIncome;
+        }
+
         let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую!', 'Первый, Второй, Третий');
         appData.addExpenses = addExpenses.toLowerCase().split(',');
-        appData.deposit = confirm('Есть ли у вас депозит в банке?');
+
         for (let i = 0; i < 2; i++) {
-            // валидация вводимых значений возвращает цену, если она валидна
-            let myCountPrice = function() {
-                let count;
-                do {
-                    count = prompt('Во сколько это обойдется?', 30);
-                }
-                while (isNaN(count) || count == '' || count == null);
-                return count;
-            };
-            // при первой итерации задаем первый вопрос и записываем данные в обьект expenses
-            if (i === 0) {
-                appData.expenses[prompt('Какие обязательные ежемесячные расходы у вас есть?', 'Кофе')] = myCountPrice();
-                // при второй итерации задаем второй вопрос и записываем данные в обьект expenses
-            } else if (i === 1) {
-                appData.expenses[prompt('Какие обязательные ежемесячные расходы у вас есть?', 'Пицца')] = myCountPrice();
+            let itemExpenses;
+            let cashExpenses;
+            // При каждой итерации задаем вопрос 
+            do {
+                itemExpenses = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'Кофе');
             }
+            while (Number(itemExpenses) || itemExpenses === '' || itemExpenses === null);
+            // и еще один вопрос о стоимости с последующей валидацией
+            do {
+                cashExpenses = prompt('Во сколько это обойдется?', 30);
+            }
+            while (isNaN(cashExpenses) || cashExpenses === '' || cashExpenses === null);
+            // создаем параметр обьекта expenses 
+            appData.expenses[itemExpenses] = cashExpenses;
         }
+        appData.deposit = confirm('Есть ли у вас депозит в банке?');
     },
+
     // метод подчета и возврата суммы всех расходов
     getExpensesMonth: function() {
         for (let key in appData.expenses) {
             appData.expensesMonth += +appData.expenses[key];
         }
     },
+
     // метод подчета накоплений за месяц
     getBudget: function() {
         // Высчитываем бюджет на месяц, выводим данные
@@ -62,10 +82,12 @@ let appData = {
         // Высчитываем дневной бюджет с учетом budgetMonth
         appData.budgetDay = Math.floor(Number(appData.budgetMonth / 30));
     },
+
     // метод подчета периода достижения цели
     getTargetMonth: function() {
         return Math.ceil(appData.mission / appData.budgetMonth);
     },
+
     // На основании budgetDay определяем уровень дохода 
     getStatusIncome: function() {
         switch (true) {
@@ -78,6 +100,25 @@ let appData = {
             default:
                 return ('Что то пошло не так');
         }
+    },
+
+    getInfoDeposit: function() {
+        if (appData.deposit) {
+            // валидация процентной ставки
+            do {
+                appData.percentDeposit = prompt('Какой годовой процент?', '10');
+            }
+            while (isNaN(appData.percentDeposit) || appData.percentDeposit === '' || appData.percentDeposit === null);
+            // валидация суммы депозита
+            do {
+                appData.moneyDeposit = prompt('Какая сумма заложена?', 10000);
+            }
+            while (isNaN(appData.moneyDeposit) || appData.moneyDeposit === '' || appData.moneyDeposit === null);
+        }
+    },
+
+    calcSavedMoney: function() {
+        return appData.budgetMonth * appData.period;
     }
 };
 
@@ -85,6 +126,8 @@ let appData = {
 appData.asking();
 appData.getExpensesMonth();
 appData.getBudget();
+appData.getInfoDeposit();
+
 // выводим в консоль расходы ха месяц
 console.log('Расходы за месяц составляют ' + appData.expensesMonth + 'руб.');
 // выводим в консоль срок достижения цели в месяцах
@@ -92,6 +135,21 @@ console.log((appData.getTargetMonth() >= 0) ?
     'Cрок достижения цели: ' + appData.getTargetMonth() + ' мес.' : 'Цель не будет достигнута');
 // Выводим уровень заработка
 console.log(appData.getStatusIncome());
+
+// Выводим в консоль возможные доходы и расходы в одну строку, разделенные запятыми
+
+for (let key in appData.income) {
+    // принимаем ключ, выводим первый символ строки в верхнем регистре, все остальные начиная со второго символа в нижнем регистре
+    // добавляем в массив getStringArr
+    key = key.charAt(0).toUpperCase() + key.substring(1).toLowerCase();
+    getStringArr.push(key);
+}
+for (let key in appData.expenses) {
+    key = key.charAt(0).toUpperCase() + key.substring(1).toLowerCase();
+    getStringArr.push(key);
+}
+
+console.log('Возможные доходы и расходы: ' + getStringArr.join(', '));
 
 console.log('Наша программа включает в себя данные: ');
 for (let key in appData) {
