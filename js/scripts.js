a61e9a5847121a17014e3d0e1c4cb8f1f6ee0515
Velmoren <velmoren@yahoo.com>
2019-06-23 21:46:21 +0300

    усложненное задание 3

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+'no strict'
+const lang = 'ru';
+const namePerson = 'Артем';
+
+// задание 1 'a'
+if (lang === 'ru') {
+  console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+} else if (lang === 'en') {
+  console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+} else {
+  console.log('укажите язык (ru или en)');
+}
+
+// задание 1 'b'
+switch (lang) {
+  case 'ru':
+    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+    break;
+  case 'en':
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+    break;
+  default:
+    console.log('укажите язык (ru или en)');
+}
+
+// задание 1 'c'
+const language = {
+  ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+  en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+}
+console.log((language[lang]).join(', '));
+
+// задание 2
+console.log((namePerson === 'Артем') ? 'директор' : (namePerson === 'Максим') ? 'преподаватель' : 'студент');
