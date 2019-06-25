e804427766d452036e47e6cb9709b3afd7086b1a
velmoren <velmoren@yahoo.com>
2019-06-25 12:01:54 +0300

    выполнил усложненное 7

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,21 @@
 'use strict';
 
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-let newDiv;
-let date = new Date();
+let date;
+let newSpan = document.createElement('span');
 
-// функция вывода на экран обычного текста
-let getDayOfRest = function(day) {
-    newDiv = document.createElement('span');
-    newDiv.innerHTML = day + '<br>';
-    document.body.appendChild(newDiv);
-};
-// функция вывода на экран текста курсивом
-let getWeekday = function(day) {
-    newDiv = document.createElement('i');
-    newDiv.innerHTML = day + '<br>';
-    document.body.appendChild(newDiv);
-};
-// функция вывода на экран жирного текста
-let getNowDay = function(day) {
-    newDiv = document.createElement('span');
-    newDiv.innerHTML = '<b>' + day + '</b><br>';
-    document.body.appendChild(newDiv);
-};
-
-for (let i = 0; i < week.length; i++) {
-    // в цикле первым делом проверяем, совпадает ли индекс массива с номером реального дня недели и если так,
-    //  то передаем значение массива в функцию вывода жирным цветом
-    if ([i] == date.getDay()) {
-        getNowDay(week[i]);
-        console.log(week[i] + ' now day');
+// функция добавляет 0 перед значением, если это значение от 0 до 9
+let addingZero = function(e) {
+    if (e >= 0 && e < 10) {
+        return '0' + e;
     } else {
-        // если индекс массива не совпадает с реальным днем недели - проверяем на выходной день и выводим в зависимости от ответа
-        if ([i] == 5 || [i] == 6) {
-            getWeekday(week[i]);
-        } else {
-            getDayOfRest(week[i]);
-        }
+        return e;
     }
-}
+};
+
+// каждую секунду Date обновляется и выводит на экран уже новое значение
+setInterval(function() {
+    date = new Date();
+    newSpan.innerHTML = (addingZero(date.getHours()) + ':' + addingZero(date.getMinutes()) + ':' + addingZero(date.getSeconds()) + ' ' +
+        addingZero(date.getDate()) + '.' + addingZero((date.getMonth() + 1)) + '.' + date.getFullYear());
+    document.body.appendChild(newSpan);
+}, 1000);
