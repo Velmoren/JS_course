82aed53cd60618945d06dd0a0cec1ce63efd398b
velmoren <velmoren@yahoo.com>
2019-07-23 17:53:24 +0300

    practice_03 complete

--- src/js/script.js
+++ src/js/script.js
@@ -315,16 +315,79 @@ window.addEventListener('DOMContentLoaded', function() {
 	ourParty();
 
 	// calculator
-	const calc = () => {
-		const calcBlock = document.querySelector('.calc-block');
+	const calc = (price = 100) => {
+		const calcBlock = document.querySelector('.calc-block'),
+			calcType = document.querySelector('.calc-type'),
+			calcSquare = document.querySelector('.calc-square'),
+			calcDay = document.querySelector('.calc-day'),
+			calcCount = document.querySelector('.calc-count'),
+			totalValue = document.getElementById('total');
 
+		// разрешаем вводить только цифры
 		calcBlock.addEventListener('keyup', (event) => {
-			let target = event.target;
+			const target = event.target;
 			target.value = target.value.replace(/[^\d,]/g, '');
 		});
 
+		const countSum = () => {
+			let total = 0,
+				countValue = 1,
+				dayValue = 1,
+				count = 0;
+			const typeValue = +calcType.options[calcType.selectedIndex].value,
+				squareValue = +calcSquare.value;
+
+			if (calcCount.value > 1) {
+				countValue += (calcCount.value - 1) / 10;
+			}
+			if (calcDay.value == 0) {
+				dayValue = 1;
+			} else if (calcDay.value < 5) {
+				dayValue *= 2;
+			} else if (calcDay.value < 10) {
+				dayValue *= 1.5;
+			}
+
+			// если наши переменные существуют
+			if (typeValue && squareValue) {
+				total = price * typeValue * squareValue * countValue * dayValue;
+			}
+			// интервал прокрутки стоимости 
+			let totalInterval = setInterval(() => {
+
+				if (count === total) {
+					clearInterval(totalInterval);
+				}
+				totalValue.textContent = count;
+				count = count + 1;
+			}, 1);
+
+		};
+
+		calcBlock.addEventListener('change', (event) => {
+			const target = event.target;
+
+			// if (target.matches('.calc-block') ||
+			// 	target.matches('.calc-type') ||
+			// 	target.matches('.calc-square') ||
+			// 	target.matches('.calc-day')) {
+			// 	console.log('adasda');
+
+			// }
+
+			// if (target === calcType || target === calcSquare || target === calcDay || target === calcCount) {
+			// 	console.log('asdasd');
+
+			// }
+
+			// если наш target имеет тэг select или input
+			if (target.matches('select') || target.matches('input')) {
+				countSum();
+			}
+
+		});
 	};
-	calc();
+	calc(100);
 
 	// scroll
 	const smothScroll = () => {
